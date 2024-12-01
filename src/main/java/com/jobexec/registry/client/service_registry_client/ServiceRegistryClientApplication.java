2cf22590bae806c62fa69353afe9c3afedafd5a2
Piyush Soni <piyushsoni421@gmail.com>
2024-12-02 00:26:16 +0530

    service-registry-client

---
+++ src/main/java/com/jobexec/registry/client/service_registry_client/ServiceRegistryClientApplication.java
@@ -0,0 +1,13 @@
+package com.jobexec.registry.client.service_registry_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobexec/registry/client/service_registry_client/ServiceRegistryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobexec.registry.client.service_registry_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
